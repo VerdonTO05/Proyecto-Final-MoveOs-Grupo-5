1ebec672b365b397c6e12fa208569bf56308c021
DESKTOP-2IK9HPO\IreneOD <ireneod20@gmail.com>
2025-11-24 08:47:12 +0100

    feat: Se añade icono ojo para poder visualizar la contraseña en los formularios de registro e inicio de sesión, para mayor accesibilidad.

--- project-root/public/js/main.js
+++ project-root/public/js/main.js
@@ -15,3 +15,21 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('mode', isDark ? 'dark' : 'light');
   });
 });
+
+/**
+ * Función que permite visualizar la contraseña en el registro
+ */
+function togglePassword() {
+    const passwordInput = document.getElementById("password");
+    const icon = document.querySelector(".toggle-password i");
+
+    if (passwordInput.type === "password") {
+        passwordInput.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        passwordInput.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
