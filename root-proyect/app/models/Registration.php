26bdffefb58f567eafbcaf90ba5d399da14d7e30
Irene <ireneod20@gmail.com>
2025-12-15 09:29:25 +0100

    feat: Creación archivos modelos

---
+++ root-proyect/app/models/Registration.php
@@ -0,0 +1,26 @@
+<?php
+class Registration{
+    public $id;
+    public $activity_id;
+    public $participant_id;
+    public $registration_date;
+
+    public function getRegistrations(){
+
+    }
+
+    public function getRegistrationById(){
+
+    }
+
+    public function createRegistration(){
+
+    }
+
+    public function deleteRegistration(){
+        
+    }
+
+}
+
+?>
